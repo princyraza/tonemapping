1c7b3fea67f52fa832897649073c037c367d4ad4
Princy Razafimanantsoa <princy.raza@gmail.com>
2013-05-17 17:40:13 -0700

    new menu added we can open a new image file TODO: resize/zoom in/zoom out image

--- toneMapping/src/model/ToneMapping.java
+++ toneMapping/src/model/ToneMapping.java
@@ -1,7 +1,46 @@
 package model;
 
-public class ToneMapping {
+import java.util.Observable;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.highgui.Highgui;
+
+public class ToneMapping extends Observable {
 	
+	private Mat originalImage;
 	
+	public ToneMapping(Mat originalImage)
+	{
+		this.originalImage=originalImage;
+	}
+	
+	public void brightness(int beta)
+	{
+	    double alpha=0; //contrast not changed
+	    double[] oldData=new double[3];
+	    double[] newData=new double[3];
+	    // Load the native library.
+	    System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	    
+		Mat newImage = new Mat(originalImage.size(), originalImage.type());
+		for( int y = 0; y < originalImage.rows(); y++ )
+		{ 
+			for( int x = 0; x < originalImage.cols(); x++ )
+		    { 
+				oldData=originalImage.get(y, x);
+				
+				newData[0]=alpha*oldData[0]+beta;
+				newData[1]=alpha*oldData[1]+beta;
+				newData[2]=alpha*oldData[2]+beta;
+				originalImage.put(x, y, newData);
+		    }
+	    }
+		originalImage.convertTo(newImage, originalImage.type());
+		
+	    String filename = "ressources/newImage.png";
+	    System.out.println(String.format("Writing %s", filename));
+	    Highgui.imwrite(filename, newImage);
+	}
 
 }
--- toneMapping/src/view/MainFrame.java
+++ toneMapping/src/view/MainFrame.java
@@ -3,10 +3,15 @@ package view;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import java.awt.BorderLayout;
+import java.awt.Image;
+import java.awt.MenuItem;
 
 import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
@@ -14,13 +19,32 @@ import javax.swing.JLabel;
 import java.awt.FlowLayout;
 import javax.swing.BoxLayout;
 import java.awt.Component;
+import java.awt.event.KeyEvent;
+
 import javax.swing.SwingConstants;
 import javax.swing.JToolBar;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.IOException;
 
 public class MainFrame extends JFrame implements Observer
 {
 	public static final int DEFAULT_WIDTH = 800;
     public static final int DEFAULT_HEIGHT = 600;
+    
+    private JPanel imagePanel;
+    private JPanel topMenu;
+    private JPanel rightPanel;
+    private JLabel contrastLabel;
+    private JSlider brightSlider;
+    private ImageIcon imgIcon;
+    private JLabel image;
+    private JMenuBar menuBar;
+    private JMenu menu;
     
 	public MainFrame() {
 		
@@ -29,29 +53,57 @@ public class MainFrame extends JFrame implements Observer
         
 		getContentPane().setLayout(new BorderLayout(0, 0));
 		
-		JPanel imagePanel = new JPanel();
+		imagePanel = new JPanel();
 		getContentPane().add(imagePanel, BorderLayout.CENTER);
 		
-		JPanel topMenu = new JPanel();
+		topMenu = new JPanel();
 		getContentPane().add(topMenu, BorderLayout.NORTH);
 		
-		JPanel leftPanel = new JPanel();
-		getContentPane().add(leftPanel, BorderLayout.EAST);
-		leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
+		rightPanel = new JPanel();
+		getContentPane().add(rightPanel, BorderLayout.EAST);
+		rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
 		
-		JLabel contrastLabel = new JLabel("Brightness");
+		contrastLabel = new JLabel("Brightness");
 		contrastLabel.setAlignmentY(Component.LEFT_ALIGNMENT);
-		leftPanel.add(contrastLabel);
+		rightPanel.add(contrastLabel);
 		
-		JSlider brightSlider = new JSlider();
+		brightSlider = new JSlider();
 		brightSlider.setAlignmentX(Component.LEFT_ALIGNMENT);
-		leftPanel.add(brightSlider);
+		rightPanel.add(brightSlider);
 		
-		ImageIcon imgIcon = new ImageIcon("/Users/Princy/Documents/workspace/toneMapping/ressources/lena.png");
-		JLabel image = new JLabel(imgIcon);
+		imgIcon = new ImageIcon("/Users/Princy/git/tonemapping/toneMapping/ressources/lena.png");
+		image = new JLabel(imgIcon);
 		imagePanel.add(image);
 		
-		int i =0;
+		menuBar = new JMenuBar();
+		this.setJMenuBar(menuBar);
+		
+		menu = new JMenu("File");
+		menu.setMnemonic(KeyEvent.VK_A);
+		menu.getAccessibleContext().setAccessibleDescription(
+		        "The only menu in this program that has menu items");
+		menuBar.add(menu);
+		
+		JMenuItem openImage = new JMenuItem("open");
+		openImage.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				final JFileChooser fc = new JFileChooser();
+				int returnVal = fc.showOpenDialog(MainFrame.this);
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+		            File file = fc.getSelectedFile();
+		            try {
+						image.setIcon(new ImageIcon(ImageIO.read(file)));
+					} catch (IOException e) {
+						JOptionPane.showMessageDialog(MainFrame.this,
+							    "Eggs are not supposed to be green.",
+							    "Inane error",
+							    JOptionPane.ERROR_MESSAGE);
+						e.printStackTrace();
+					}
+		        }
+			}
+		});
+		menu.add(openImage);
 		
 	}
 
